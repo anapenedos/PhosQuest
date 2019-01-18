8d0a44926e579fa550d33d190b2a455b5867d292
Carmen <bt18633@qmul.ac.uk>
2019-01-18 08:41:42 +0000

    Separated test data to another file for import

---
+++ test_data.py
@@ -0,0 +1,14 @@
+# Set up list of browse data (Just testing!!) could be imported as a list of dicts (or python object??)
+browse_data = [
+    {'accession':'Q16836',
+        'name': 'Hydroxyacyl-coenzyme A dehydrogenase',
+        'Molecular_Function':'NAD+_binding',
+        'Cellular_component':'cytosol',
+        'Biological_Process': 'Fatty acid beta oxidation',
+        'link':'https://www.uniprot.org/uniprot/Q16836'},
+    {'accession': 'P35914',
+        'name': 'Hydroxymethylglutaryl-CoA lyase',
+        'Molecular_Function': 'carboxylic acid binding',
+        'Cellular_component': 'cytosol',
+        'Biological_Process': 'ketone body biosynthetic process',
+         'link': 'https://www.uniprot.org/uniprot/P35914'}]
