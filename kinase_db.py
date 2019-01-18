32916c527ee10f38583e96e7c60455b7bc3de411
Carmen <bt18633@qmul.ac.uk>
2019-01-18 23:02:28 +0000

    Added login_register_form.py, placeholder login and register templates

--- kinase_db.py
+++ kinase_db.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template,url_for
 from test_data import browse_data
+from login_register_form import RegistrationForm, LoginForm
 
 # create instance of FLASK class. __name__ is name of module.
 app = Flask(__name__)
+
+#security for site secret key generated in python using secrets module
+# token hex method
+app.config['SECRET_KEY'] = '7302b128c277227526063af5c73ec426'
 
 # create route for home page works with / and /home page address
 # uses home html template
@@ -28,6 +33,20 @@ def search():
 def upload():
     # render template with browse data and title for browse page
     return render_template('upload.html',title='Upload')
+
+# Test route for now may or may not want in final site??
+@app.route("/register")
+def register():
+    # Create instance of register form
+    form = RegistrationForm()
+    return render_template('register.html',title = 'Register',form = form)
+
+#Test route for now may or may not want in final site??
+@app.route("/login")
+def login():
+    # Create instance of register form
+    form = LoginForm()
+    return render_template('login.html',title = 'Login',form = form)
 
 # if run from python directly run app in debug mode
 if __name__ == '__main__':
---
+++ login_register_form.py
@@ -0,0 +1,22 @@
+"""Test form for login with password"""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField, BooleanField
+from wtforms.validators import DataRequired,Length,EqualTo
+
+#set up registration form class and required fields
+#may need to add email in here if we actually use this code.
+class RegistrationForm(FlaskForm):
+    username = StringField('username',
+                           validators=[DataRequired(), Length(min=2, max=30)])
+    password = PasswordField('password',validators=[DataRequired()])
+    password_confirm = PasswordField('confirm_password',validators=[DataRequired(),EqualTo('password')])
+
+    submit = SubmitField("Create login")
+
+class LoginForm(FlaskForm):
+    username = StringField('username',
+                           validators=[DataRequired(), Length(min=2, max=30)])
+    password = PasswordField('password',validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField("Login")
