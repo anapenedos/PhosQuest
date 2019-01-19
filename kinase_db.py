279fd348b0519ccc3b7852392ba16d9b1975d7fb
Carmen <bt18633@qmul.ac.uk>
2019-01-19 13:13:49 +0000

    Tidied up code, added """ comments for pycharm formatting of functions

--- kinase_db.py
+++ kinase_db.py
@@ -1,52 +1,61 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for
 from test_data import browse_data
 from login_register_form import RegistrationForm, LoginForm
 
 # create instance of FLASK class. __name__ is name of module.
 app = Flask(__name__)
 
-#security for site secret key generated in python using secrets module
+# security for site secret key generated in python using secrets module
 # token hex method
 app.config['SECRET_KEY'] = '7302b128c277227526063af5c73ec426'
+
 
 # create route for home page works with / and /home page address
 # uses home html template
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    """render home page"""
+    return render_template('home.html', title='home')
+
 
 # route for browse page with browse template
 @app.route("/browse")
 def browse():
-    # render template with browse data and title for browse page
-    return render_template('browse.html', browse_data=browse_data, title="Search")
+    """render template with browse data and title for browse page"""
+    return render_template('browse.html', browse_data=browse_data,
+                           title="Search")
+
 
 # route for browse page with browse template
 @app.route("/search")
 def search():
-    # render template with browse data and title for browse page
-    return render_template('search.html',  title="Search")
+    """render template with browse data and title for browse page"""
+    return render_template('search.html', title="Search")
+
 
 # route for browse page with browse template
 @app.route("/upload")
 def upload():
-    # render template with browse data and title for browse page
-    return render_template('upload.html',title='Upload')
+    """render template with browse data and title for browse page"""
+    return render_template('upload.html', title='Upload')
+
 
 # Test route for now may or may not want in final site??
 @app.route("/register")
 def register():
-    # Create instance of register form
+    """Create instance of register form"""
     form = RegistrationForm()
-    return render_template('register.html',title = 'Register',form = form)
+    return render_template('register.html', title='Register', form=form)
 
-#Test route for now may or may not want in final site??
+
+# Test route for now may or may not want in final site??
 @app.route("/login")
 def login():
     # Create instance of register form
     form = LoginForm()
-    return render_template('login.html',title = 'Login',form = form)
+    return render_template('login.html', title='Login', form=form)
+
 
 # if run from python directly run app in debug mode
 if __name__ == '__main__':
--- login_register_form.py
+++ login_register_form.py
@@ -1,22 +1,31 @@
 """Test form for login with password"""
 
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField, BooleanField
-from wtforms.validators import DataRequired,Length,EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo
 
-#set up registration form class and required fields
-#may need to add email in here if we actually use this code.
+"""set up registration form class and required fields
+we need to add email in here if we actually use this code."""
+
+
 class RegistrationForm(FlaskForm):
+    """Registration form class"""
     username = StringField('username',
-                           validators=[DataRequired(), Length(min=2, max=30)])
-    password = PasswordField('password',validators=[DataRequired()])
-    password_confirm = PasswordField('confirm_password',validators=[DataRequired(),EqualTo('password')])
+                           validators = [DataRequired(),
+                                       Length(min=2, max=30)])
+    password = PasswordField('password', validators = [DataRequired()])
+    password_confirm = PasswordField('confirm_password',
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
 
     submit = SubmitField("Create login")
 
+
 class LoginForm(FlaskForm):
+    """Login form Class"""
     username = StringField('username',
-                           validators=[DataRequired(), Length(min=2, max=30)])
-    password = PasswordField('password',validators=[DataRequired()])
+                           validators = [DataRequired(),
+                                       Length(min=2, max=30)])
+    password = PasswordField('password', validators = [DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField("Login")
