85c11baccab11cc6e9d0fcce4db3d62086ee649b
Carmen <bt18633@qmul.ac.uk>
2019-01-27 08:42:23 +0000

    Query script WORKING!!!

--- service_scripts/query_testdb.py
+++ service_scripts/query_testdb.py
@@ -2,17 +2,20 @@ import sys
 import os
 #so we can access the scripts..
 sys.path.insert(0, 'data_access')
-from sqlalchemy_declarative import Kinase, Substrate, Inhibitor
+from sqlalchemy_declarative import Base, Kinase
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+#create db path using os to avoid problems with windows vs linux
 dbpath = os.path.join('database', 'kinases_test3_all.db')
-engine = create_engine(f'sqlite:////{ dbpath }')
-
-Kinase.metadata.bind = engine
+engine = create_engine(f'sqlite:///{ dbpath }')
+Base.metadata.bind = engine
 DBsession = sessionmaker()
 DBsession.bind = engine
-session = DBsession
+session = DBsession()
 
-session.query(Kinase).all()
+kinase  = session.query(Kinase).all()
+
+for item in kinase:
+    print("Kinase is " + item.kin_name + "\t Kinase Family is " + item.kin_fam)
 
