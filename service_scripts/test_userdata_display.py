55b7b50fd5c93d9793e427738a322a810eb6b52a
Carmen <bt18633@qmul.ac.uk>
2019-01-29 22:11:32 +0000

    Display whole table data no template.

--- service_scripts/test_userdata_display.py
+++ service_scripts/test_userdata_display.py
@@ -3,7 +3,7 @@ dataframe and display display columns in some form on website."""
 
 import pandas as pd
 
-
+#TODO update to pull partial data into separate tables and display
 
 def xl_to_html(excel_file):
    """function to convert pandas dataframe to html"""
