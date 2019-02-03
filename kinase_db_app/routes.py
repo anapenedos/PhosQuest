602bd99ce4a565f695848bc4c8e7f0c61a95ca95
Carmen <bt18633@qmul.ac.uk>
2019-02-03 00:24:49 +0000

    Update user data display Update flash messages

--- kinase_db_app/routes.py
+++ kinase_db_app/routes.py
@@ -43,16 +43,16 @@ def analysis():
     form = UploadForm()
      #if form validates (correct file types) save file in temp dir
     if form.validate_on_submit():
-
         try:
             f = form.data_file.data
             filename =  secure_filename(f.filename)
+            flash(f'File {filename} uploaded, please wait', 'success')
             basic_data = userdata_display.display_basic(f)
             flash(f'File {filename} successfully analysed', 'success')
             return render_template('results.html', title='Results',
                            table=basic_data[0])
 
-        except:
+        except :
             flash('Error please try again ','danger')
             return render_template('upload.html', form=form)
 
--- service_scripts/userdata_display.py
+++ service_scripts/userdata_display.py
@@ -1,18 +1,34 @@
-"""scripts to display subsets of crunched data"""
+"""scripts to format subsets of crunched data for display"""
 from service_scripts import ud_crunch
 import pandas as pd
 
 def display_basic(filename):
    """Run first two functions in ud_crunch and output
                   final corrected dataframe"""
+   # run initial filtering script
    dataframes = ud_crunch.create_filtered_dfs(filename)
+
    filtered_df = dataframes[2]# just filtered_df needed now
+
+   #run P-value correction
    p_correct = ud_crunch.correct_pvalue(filtered_df)
+
    #create subset of some columns for display
    basic_display_only = p_correct.copy()
-   basic_display_only = basic_display_only.rename(columns=
-            {'corrected_p_value': 'Corrected P-value',
-                   'neg_log10_corr_p_values': '-Log10 corrected P-value'})
+   # delete un-needed columns for display
+   basic_display_only = basic_display_only.drop(basic_display_only.columns[[3,
+                                        4, 12]], axis=1)
+
+   # remove underscores for display *****TEST ONLY WONT WORK FOR ALL???*****
+   change_columns = {'Fold_condition_over_max':'Fold condition over max',
+              'AZ20_fold_change':'Fold change',
+              'AZ20_p-value':'p-value',
+              'AZ20_ctrlCV':'control CV',
+              'AZ20_treatCV':'AZ20_treatCV',
+              'corrected_p_value': 'Corrected P-value',
+              'neg_log10_corr_p_values': '-Log10 corrected P-value'}
+
+   basic_display_only = basic_display_only.rename(change_columns)
 
    basic_display_only = basic_display_only.to_html()
    return [basic_display_only, p_correct]
