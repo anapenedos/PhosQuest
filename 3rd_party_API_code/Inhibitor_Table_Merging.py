6049f9e28b123c32d0aab73525de54cf35fe09f8
Ozan Gundogdu <ipmbogun@lshtm.ac.uk>
2019-02-16 20:07:39 +0000

    working mergind df code

--- 3rd_party_API_code/Inhibitor_Table_Merging.py
+++ 3rd_party_API_code/Inhibitor_Table_Merging.py
@@ -8,9 +8,7 @@ from data_import_scripts.db_parsing import bindingDB_human, mrc_inhib_source
 #df1 = pd.read_csv('Test_Inhibitors_MRC.csv')
 #df2 = pd.read_csv('Test_Inhibitors_BindingDB.csv')
 
-newdf = pd.merge(mrc_inhib_source,
-	    bindingDB_human[['PubChem CID', 'UniProt (SwissProt) Primary ID of Target Chain']],
-		on='PubChem CID')
+newdf = mrc_inhib_source.join(bindingDB_human, on=['PubChem CID', 'PubChem_CID'], how='left')
 
 #newdf.to_csv('test.csv')
 
