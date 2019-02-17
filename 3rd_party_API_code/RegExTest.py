e3d6cfd18ff128a5bb60a5638d6cf9136d779cc5
Ozan Gundogdu <ipmbogun@lshtm.ac.uk>
2019-02-17 16:42:08 +0000

    Example of Cellular location RegEx extraction

---
+++ 3rd_party_API_code/RegExTest.py
@@ -0,0 +1,10 @@
+import re
+import pandas as pd
+
+example = {'Subcellular location [CC]': ['SUBCELLULAR LOCATION: Mitochondrion inner membrane {ECO:0000256|RuleBase:RU000369]}']}
+
+dftest = pd.DataFrame(example, columns = ['Subcellular location [CC]'])
+dftest
+
+dftest['Subcellular location2'] = dftest['Subcellular location [CC]'].str.extract('(SUBCELLULAR LOCATION: .+(?={))', expand=True)
+dftest['Subcellular location2']
