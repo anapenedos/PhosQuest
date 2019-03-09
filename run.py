2c497b4d5f8aba9f6e7391426bfc7c787ec02318
Ana <a.r.penedos@se18.qmul.ac.uk>
2019-03-09 11:18:59 +0000

    switching off pandas chained assignments warnings

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 # run app from package in debug mode
 from PhosphoQuest_app import app
+import pandas as pd
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+pd.options.mode.chained_assignment = None
