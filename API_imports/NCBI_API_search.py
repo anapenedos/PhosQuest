47764d4bc7b7f086e5283a7b27f87bc05af6780b
Ozan Gundogdu <ipmbogun@lshtm.ac.uk>
2019-03-10 21:37:27 +0000

    working NCBI chromosomal API

--- API_imports/NCBI_API_search.py
+++ API_imports/NCBI_API_search.py
@@ -11,12 +11,14 @@ import pandas as pd
 
 ### URL saved to a .csv with specific accession numbers.
 #url_csv = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=science[journal]+AND+breast+cancer+AND+2018[pdat]'
-url_csv = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=gene&id=Q86UU0&rettype=&retmode=asn.1'
+url_csv = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=gene&id=Q86UU0&rettype=&retmode=text'
 
-print(url_csv)
+#print(url_csv)
 
 # Converted to a dataframe.
-#df = pd.read_csv(url_csv)
+df = pd.read_table(url_csv)
+row_data = df.loc[3:3]
+print(row_data)
 
 # Output as a csv.
 #df.to_csv('PubChem_output.csv')
