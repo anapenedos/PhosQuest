cc1f71c86085f3ff13804317b0ce21421453a6b8
Ana <a.r.penedos@se18.qmul.ac.uk>
2019-03-10 10:13:12 +0000

    import script broken into smaller functions

--- data_import_scripts/sqlalchemy_import.py
+++ data_import_scripts/sqlalchemy_import.py
@@ -7,6 +7,73 @@ from pandas import isnull
 from PhosphoQuest_app.data_access.db_sessions import import_session_maker
 from PhosphoQuest_app.data_access.class_functions import get_classes_key_attrs
 
+# define null-type of values that are treated differently
+NULL_VALS = [None, '', ' ', '-', 'nan', 'NaN']
+
+
+def get_key_vals(df_to_class_dict, classes_keys, row):
+    # get keys for classes in row
+    # dictionary of class to primary key attributes and key values tuples
+    new_table_keys = {}  # {class: {key_attr: key_value, ...}, ...}
+    # iterate through dict mapping df_heading: (Class, class_attr)
+    for df_heading, class_matches in df_to_class_dict.items():
+        for class_match in class_matches:
+            # df heading corresponds to class and class attribute
+            class_name = class_match[0]
+            class_attr = class_match[1]
+            # if the row contains a non-null value and the df heading
+            # contains a primary key, add key value to dict
+            if (class_attr in classes_keys[class_name]
+                and row[df_heading] not in NULL_VALS
+                and not isnull(row[df_heading])):
+                new_values = new_table_keys.setdefault(class_name, {})
+                new_values[class_attr] = row[df_heading]
+    return new_table_keys
+
+
+def get_instances(new_table_keys, session):
+    # check if records already exist in tables and obtain class instances
+    class_instances = {}  # {Class: class_instance, ...}
+    for class_name, keys_info in new_table_keys.items():
+        # create query object
+        query_res = session.query(class_name)
+        # apply filters to the query_res based on primary keys
+        for key_attr, key_value in keys_info.items():
+            query_res = query_res.filter(
+                getattr(class_name, key_attr) == key_value)
+        # given query_res was filtered on all primary keys in table, it
+        # should now list a single instance, which can be obtained with
+        # .first
+        query_res = query_res.first()
+        # create new class instance if no record retrieved by query
+        # and none of the key values is None
+        if query_res is None:
+            class_instance = class_name(**keys_info)
+            session.add(class_instance)
+        # or get the existing class instance if already in table
+        else:
+            class_instance = query_res
+        # keep track of the new instances
+        class_instances[class_name] = class_instance
+    return class_instances
+
+
+def import_attrs(class_instances, df_to_class_dict, row):
+    # get remaining attributes for each instance
+    for instance_class_name, class_instance in class_instances.items():
+        # get the class attributes
+        for df_heading, class_matches in df_to_class_dict.items():
+            for class_match in class_matches:
+                class_name = class_match[0]
+                class_attr = class_match[1]
+                if class_name == instance_class_name:
+                    attr = getattr(class_instance, class_attr, None)
+                    # if the existing instance attr is not defined, set it
+                    # to the value in the data frame
+                    if attr in NULL_VALS:
+                        setattr(class_instance, class_attr, row[df_heading])
+    return
+
 
 def import_data_from_data_frame(df, df_to_class_dict):
     """
@@ -31,9 +98,6 @@ def import_data_from_data_frame(df, df_to_class_dict):
     # get classes primary keys attributes
     classes_keys = get_classes_key_attrs(classes_in_df)
 
-    # define special type of values that are treated differently
-    undefined_values = [None, '', ' ', '-', 'nan', 'NaN']
-
     # set up a row counter
     processed_rows = 0
     total_records = len(df)
@@ -57,60 +121,15 @@ def import_data_from_data_frame(df, df_to_class_dict):
 
         # get keys for classes in row
         # dictionary of class to primary key attributes and key values tuples
-        new_table_keys = {}  # {class: {key_attr: key_value, ...}, ...}
-        # iterate through dict mapping df_heading: (Class, class_attr)
-        for df_heading, class_matches in df_to_class_dict.items():
-            for class_match in class_matches:
-                # df heading corresponds to class and class attribute
-                class_name = class_match[0]
-                class_attr = class_match[1]
-                # if the row contains a non-null value and the df heading
-                # contains a primary key, add key value to dict
-                if (class_attr in classes_keys[class_name]
-                    and row[df_heading] not in undefined_values
-                    and not isnull(row[df_heading])):
-                    new_values = new_table_keys.setdefault(class_name, {})
-                    new_values[class_attr] = row[df_heading]
+        # {class: {key_attr: key_value, ...}, ...}
+        new_table_keys = get_key_vals(df_to_class_dict, classes_keys, row)
 
         # check if records already exist in tables and obtain class instances
-        class_instances = {}  # {Class: class_instance, ...}
-        for class_name, keys_info in new_table_keys.items():
-            # create query object
-            query_res = session.query(class_name)
-            # apply filters to the query_res based on primary keys
-            for key_attr, key_value in keys_info.items():
-                query_res = query_res.filter(
-                    getattr(class_name, key_attr) == key_value)
-            # given query_res was filtered on all primary keys in table, it
-            # should now list a single instance, which can be obtained with
-            # .first
-            query_res = query_res.first()
-            # create new class instance if no record retrieved by query
-            # and none of the key values is None
-            if query_res is None:
-                class_instance = class_name(**keys_info)
-                session.add(class_instance)
-            # or get the existing class instance if already in table
-            else:
-                class_instance = query_res
-            # keep track of the new instances
-            class_instances[class_name] = class_instance
+        # {Class: class_instance, ...}
+        class_instances = get_instances(new_table_keys, session)
 
         # get remaining attributes for each instance
-        for instance_class_name, class_instance in class_instances.items():
-            # get the class attributes
-            for df_heading, class_matches in df_to_class_dict.items():
-                for class_match in class_matches:
-                    class_name = class_match[0]
-                    class_attr = class_match[1]
-                    if class_name == instance_class_name:
-                        attr = getattr(class_instance, class_attr, None)
-                        # if the existing instance attr is not defined, set it
-                        # to the value in the data frame
-                        if attr in undefined_values:
-                            setattr(class_instance,
-                                    class_attr,
-                                    row[df_heading])
+        import_attrs(class_instances, df_to_class_dict, row)
 
         # if more than one class in the data frame, set up relationships
         if len(classes_in_df) > 1:
