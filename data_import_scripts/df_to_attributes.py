cc356dacae9e16c6aea43a3be4ef7f763ac5c32c
Ozan Gundogdu <ipmbogun@lshtm.ac.uk>
2019-03-17 18:35:22 +0000

    minor updates to df_to_attributes

--- data_import_scripts/df_to_attributes.py
+++ data_import_scripts/df_to_attributes.py
@@ -120,10 +120,11 @@ uniprot_subs_to_class = {
     'Protein families'      : [(Substrate,        'subs_protein_type')]
 }
 
-pubchem_to_class = {'CID'            : [(Inhibitor, 'inhib_pubchem_cid')],
-                    'IUPACName'      : [(Inhibitor, 'inhib_full_name')],
-                    'MolecularWeight': [(Inhibitor, 'inhib_molec_weight')],
-                    'MolecularFormula: [(Inhibitor, 'inhib_molec_formula')]
+pubchem_to_class = {
+    'CID'                   : [(Inhibitor, 'inhib_pubchem_cid')],
+    'IUPACName'             : [(Inhibitor, 'inhib_full_name')],
+    'MolecularWeight'       : [(Inhibitor, 'inhib_molec_weight')],
+    'MolecularFormula'      : [(Inhibitor, 'inhib_molec_formula')]
 }
 
 """
