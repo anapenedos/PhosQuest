b30fc80dfba9230357a7bb0b35498d4eb70f7780
Ana <a.r.penedos@se18.qmul.ac.uk>
2019-03-25 21:57:08 +0000

    changes for amazon elastic beanstalk

--- application.py
+++ application.py
@@ -6,5 +6,7 @@ import pandas as pd
 if __name__ == '__main__':
     app.run(debug=True)
 
+# setting up for Amazon Elastic Beanstalk to be able to find the application
+application = app
 
 pd.options.mode.chained_assignment = None
